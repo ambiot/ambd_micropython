246e8d593a091b2649b37b3f168b8678bbda9df7
zhangzhenwu <zhangzhenwu@realtek-sg.com>
2021-07-13 21:35:48 +0800

    V1.0.1 Wlan patch_v02
    - update wlan reconnect mode

--- Release/Windows/Double-Click-Me-to-Upload.cmd
+++ Release/Windows/Double-Click-Me-to-Upload.cmd
@@ -6,6 +6,6 @@
 @echo First check your ameba's serial port and edit this file accordingly before setting your ameba to UART Download mode and upload the image
 @echo Right click on this file and select "Edit", update the correct COM port to the third last line then save and exit before re-run this program 
 pause
-amebad_image_tool.exe COM8
+amebad_image_tool.exe COM6
 :: Change COMXX above to your ameba's actual COM port number
 pause
