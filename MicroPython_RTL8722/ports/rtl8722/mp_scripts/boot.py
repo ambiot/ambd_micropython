755d8177d05c4aa403a1c07ddf4e9c8a9909063b
SimonXI <xidameng@gmail.com>
2022-06-20 17:55:35 +0800

    [SDK Update] Restructure repo to be used as MicroPython submodule - Only keep the header files and static libraries for firmware compilation - Delete all other unnecessary files and directories - Update readme and other git config files

--- MicroPython_RTL8722/ports/rtl8722/mp_scripts/boot.py
+++
@@ -1,7 +0,0 @@
-print()
-import machine, wireless ,time, modules, socket
-from wireless import WLAN
-from machine import Pin, UART, Timer, RTC, PWM, I2C, SPI, ADC, SDFS, FLASH
-from socket import SOCK
-print("[MP]: Imported all builtin libraries")
-print()
---
+++ sdk/project/realtek_amebaD_va0_example/inc/inc_hp/build_info.h
@@ -0,0 +1,7 @@
+#define UTS_VERSION "2022/03/28-17:04:49"
+#define RTL_FW_COMPILE_TIME "2022/03/28-17:04:50"
+#define RTL_FW_COMPILE_DATE "2022/03/28"
+#define RTL_FW_COMPILE_BY "zhangzhenwu"
+#define RTL_FW_COMPILE_HOST "SGSD279634-PC"
+#define RTL_FW_COMPILE_DOMAIN "realtek-sg.com"
+#define RTL_FW_COMPILER "gcc version 6.4.1 20180307 (Realtek ASDK-6.4.1 Build 2778) "
